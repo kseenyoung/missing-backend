46fe2e64de5d9f819ed7b3935a4db99cb49cba07
ksy <kseenyoung@gmail.com>
2022-11-24 11:47:40 +0900

    modi model, view, cors

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -7,8 +7,9 @@ class MChild(models.Model):
     ageNow=models.IntegerField(null=True, blank=True)
     alldressing=models.TextField(null=True, blank=True)
     occrAdd=models.TextField(null=True, blank=True)
-    occrDate=models.DateField(null=True, blank=True)
-    writngTarget=models.TextField(blank=True)
+    occrDate=models.TextField(null=True, blank=True)
+    writngTarget=models.TextField(null=True, blank=True)
+    inform=models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'myapp',
+    'corsheaders' #CORS
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000',
+                         'http://localhost:3000']
+CORS_ALLOW_CREDENTIALS = True
